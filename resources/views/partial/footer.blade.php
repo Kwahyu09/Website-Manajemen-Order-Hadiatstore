3ed5a7f058ae4a37b815a3a3ae51890b6b20ec37
unknown <krisnawahyudi2017@gmail.com>
2023-12-20 14:03:11 +0700

    memperbaiki halaman home pengguna user

---
+++ resources/views/partial/footer.blade.php
@@ -0,0 +1,117 @@
+<footer id="footer" class="overflow-hidden">
+    <div class="container">
+        <div class="row">
+            <div class="footer-top-area">
+                <div class="row d-flex flex-wrap justify-content-between">
+                    <div class="col-lg-3 col-sm-6 pb-3">
+                        <div class="footer-menu">
+                            <img src="buket/logo.png" class="ml-5" style="align-content: center;" width="150px" alt="logo">
+                            <p>Toko Buket Bunga yang ada diBengkulu bertepatan di Jalan Nangka 1 dan di Gang Setia III Unib
+                                Belakang. Bisa Request Warna Balon dan Free Greeting Card</p>
+                            <div class="social-links">
+                                <ul class="d-flex list-unstyled">
+                                    <li>
+                                        <a href="#">
+                                            <svg class="facebook">
+                                                <use xlink:href="#facebook" />
+                                            </svg>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <svg class="instagram">
+                                                <use xlink:href="#instagram" />
+                                            </svg>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <svg class="twitter">
+                                                <use xlink:href="#twitter" />
+                                            </svg>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <svg class="linkedin">
+                                                <use xlink:href="#linkedin" />
+                                            </svg>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <svg class="youtube">
+                                                <use xlink:href="#youtube" />
+                                            </svg>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-sm-6 pb-3">
+                        <div class="footer-menu text-uppercase">
+                            <h5 class="widget-title pb-2">Quick Links</h5>
+                            <ul class="menu-list list-unstyled text-uppercase">
+                                <li class="menu-item pb-2">
+                                    <a href="#">Home</a>
+                                </li>
+                                <li class="menu-item pb-2">
+                                    <a href="#cara-order">Cara Order</a>
+                                </li>
+                                <li class="menu-item pb-2">
+                                    <a href="#produk1">Produk1</a>
+                                </li>
+                                <li class="menu-item pb-2">
+                                    <a href="#produk2">Produk2</a>
+                                </li>
+                                <li class="menu-item pb-2">
+                                    <a href="#warna">Warna Ready</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6 pb-3">
+                        <div class="footer-menu contact-item">
+                            <h5 class="widget-title text-uppercase pb-2">Lokasi</h5>
+                            <p><i class="bi bi-geo-alt"></i><a href="https://maps.app.goo.gl/Rp3tXJS8AyWCJ1yS8">Jalan Nangka 1, Panorama</a></p>
+                            <p><i class="bi bi-geo-alt"></i><a href="https://maps.app.goo.gl/qohBugmMGuVkWmfQ8">Gang Setia III Unib Belakang</a>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6 pb-3">
+                        <div class="footer-menu contact-item">
+                            <h5 class="widget-title text-uppercase pb-2">Kontak Kami</h5>
+                            <p><a href="https://wa.me/6285658657014">Admin Panorama</a></p>
+                            <p><a href="https://wa.me/6285664825473">Admin Unib Belakang</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr>
+</footer>
+<div id="footer-bottom">
+    <div class="container">
+        <div class="row d-flex flex-wrap justify-content-between">
+            <div class="col-md-4 col-sm-6">
+                <div class="payment-method d-flex">
+                    <p>Payment options:</p>
+                    <div class="card-wrap ps-2">
+                        <img src="/images/visa.jpg" alt="visa">
+                        <img src="/images/mastercard.jpg" alt="mastercard">
+                        <img src="/images/paypal.jpg" alt="paypal">
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div class="copyright">
+                    <p>© Copyright 2023 Hadiat.Store Design by <a
+                            href="https://kwahyu09.github.io/">Krisna Wahyudi</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/partial/navbar.blade.php
@@ -0,0 +1,48 @@
+<nav id="header-nav" class="navbar navbar-expand-lg px-3 mb-3">
+    <div class="container-fluid">
+        <div style="text-align: end;">
+        <a class="navbar-brand" href="/">
+            <img src="/buket/logo.png" style="align-content:flex-end" width="110px" class="logo ml-5">
+        </a>
+        </div>
+        <button class="navbar-toggler d-flex d-lg-none order-3 p-2" type="button" data-bs-toggle="offcanvas"
+            data-bs-target="#bdNavbar" aria-controls="bdNavbar" aria-expanded="false" aria-label="Toggle navigation">
+            <svg class="navbar-icon">
+                <use xlink:href="#navbar-icon"></use>
+            </svg>
+        </button>
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="bdNavbar" aria-labelledby="bdNavbarOffcanvasLabel">
+            <div class="offcanvas-header px-4 pb-0">
+                <a class="navbar-brand" href="index.html">
+                    <img src="/images/logo.png" class="logo">
+                </a>
+                <button type="button" class="btn-close btn-close-black" data-bs-dismiss="offcanvas" aria-label="Close"
+                    data-bs-target="#bdNavbar"></button>
+            </div>
+            <div class="offcanvas-body">
+                <ul id="navbar"
+                    class="navbar-nav text-uppercase justify-content-end align-items-center flex-grow-1 pe-3">
+                    <li class="nav-item">
+                        <a class="nav-link me-4 active" href="#home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link me-4" href="#cara-order">Cara Order</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link me-4" href="#produk1">Produk1</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link me-4" href="#produk2">Produk2</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link me-4" href="#warna">Warna Ready</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="btn" style="padding: 10px; background-color: rgb(51, 65, 166); color: white;"
+                            href="/login" role="button">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
